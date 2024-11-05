de03a918544100804ff78fd9794926529b9c96f4
elenipage <elenipage@icloud.com>
2024-11-05 14:11:58 +0000

    Add featured article to home page as well as toggle

---
+++ src/components/MainArticleCard.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom"
+import dateFormat from "dateformat"
+
+export function MainArticleCard(props) {
+    const { article } = props
+
+    const date = dateFormat(article.created_at, "dddd, mmmm dS, yyyy, HH:MM")
+
+    return (
+        <>
+        <h2>Featured</h2>
+        <Link className="main-card" to={`articles/${article.article_id}`}>
+            <img src={article.article_img_url}></img>
+
+            <section className="main-card-text">
+                <h2>{article.title}</h2>
+                <p>{date}</p>
+                <br/>
+                <p>Topic: {article.topic}</p>
+                <p>Votes: {article.votes} Comments: {article.comment_count}</p>
+            </section>
+        </Link>
+        </>
+        
+        
+    )
+}
