060b60fdd0aa53736a3897f876e7bee32ccb0bae
elenipage <elenipage@icloud.com>
2024-11-06 10:06:09 +0000

    Add additional functionality to article votes

--- src/components/VoteChanger.jsx
+++ src/components/VoteChanger.jsx
@@ -1,25 +1,66 @@
 import { incrementVotes } from "../api";
+import { useState, useEffect } from "react";
 
 export function VoteChanger(props) {
   const { id, setVotes } = props;
+  const [upVoted, setUpvoted] = useState(false);
+  const [downVoted, setDownvoted] = useState(false);
+
+  useEffect(() => {
+    setUpvoted(JSON.parse(localStorage.getItem("upVoted") || "false"));
+    setDownvoted(JSON.parse(localStorage.getItem("downVoted") || "false"));
+  }, []);
 
   function handleChange(event) {
     event.preventDefault();
     const num = Number(event.target.value);
-    incrementVotes(id, num).then(() => {
-      setVotes((currentVotes) => {
-        return currentVotes + num;
-      });
-    });
-  }
+
+    if (num > 0) {
+      if (upVoted) {
+        incrementVotes(id, -1).then(() => {
+          setUpvoted(false);
+          localStorage.setItem("upVoted", JSON.stringify(false));
+          setVotes((currentVotes) => currentVotes - 1);
+        });
+      } else {
+        incrementVotes(id, 1).then(() => {
+          setUpvoted(true);
+          localStorage.setItem("upVoted", JSON.stringify(true));
+          localStorage.setItem("downVoted", JSON.stringify(false));
+          setVotes((currentVotes) => currentVotes + 1);
+        });
+      }
+    }
   
+    else {
+      if (downVoted) {
+        incrementVotes(id, 1).then(() => {
+          setDownvoted(false);
+          localStorage.setItem("downVoted", JSON.stringify(false));
+          setVotes((currentVotes) => currentVotes + 1);
+        });
+      } else {
+        incrementVotes(id, -1).then(() => {
+          setDownvoted(true);
+          localStorage.setItem("downVoted", JSON.stringify(true));
+          localStorage.setItem("upVoted", JSON.stringify(false));
+          setVotes((currentVotes) => currentVotes - 1);
+        });
+      }
+    }
+  }
+
   return (
-    <p>
-      <button value={1} onClick={handleChange}>
-        👍
-      </button> <button value={-1} onClick={handleChange}>
-        👎
-      </button>
-    </p>
+    <>
+      <p>
+        <button value={1} disabled={downVoted} onClick={handleChange}>
+          👍
+        </button>{" "}
+        <button value={-1} disabled={upVoted} onClick={handleChange}>
+          👎
+        </button>
+      </p>
+      {upVoted || downVoted ? <p>Thanks for voting!</p> : null}
+    </>
   );
 }
