e5c3a7c2f8cbd139705b0687ad2616ccbdd86344
elenipage <elenipage@icloud.com>
2024-11-06 19:42:54 +0000

    Add message to indicate when there are no article comments

--- src/components/comments/CommentList.jsx
+++ src/components/comments/CommentList.jsx
@@ -7,12 +7,17 @@ export function CommentList(props) {
   const [comments, setComments] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
+  const [noComments, setNoComments] = useState(false);
 
   useEffect(() => {
+    setNoComments(false);
     setIsLoading(true);
     setIsError(false);
     fetchCommentsByArticle(id)
       .then((data) => {
+        if (data.length === 0) {
+          setNoComments(true);
+        }
         setComments(data);
         setIsLoading(false);
       })
@@ -31,10 +36,22 @@ export function CommentList(props) {
 
   return (
     <section className="comment-container">
-        {newComment ? <div className="new-comment-container">
+      {newComment ? (
+        <div className="new-comment-container">
           <h3>Comment Posted!</h3>
-          <CommentCard id="new-comment" key={newComment.comment_id} comment={newComment}/>
-          </div> : null }
+          <CommentCard
+            id="new-comment"
+            key={newComment.comment_id}
+            comment={newComment}
+          />
+        </div>
+      ) : null}
+      {noComments ? (
+        <section className="no-comment-msg">
+          <h4>Sorry, no comments yet...</h4>
+          <p>Why not have your say?</p>
+        </section>
+      ) : null}
       {comments.map((comment) => {
         return <CommentCard key={comment.comment_id} comment={comment} />;
       })}
