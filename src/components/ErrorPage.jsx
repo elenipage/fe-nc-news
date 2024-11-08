72a0f0f889e4ef8e92954f3368a4e26fa6183483
elenipage <elenipage@icloud.com>
2024-11-08 08:49:19 +0000

    Add default error message

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -7,30 +7,32 @@ import { useEffect } from "react";
 
 export function ErrorPage(props) {
   const { error } = props;
-  let msg = "";
+  let msg = "something went wrong";
   let icon = null;
-
-  if (error.status === 404) {
-    msg = "we couldn't find that";
-    icon = <ExploreOff sx={{marginBottom: 3}} fontSize="large"/>;
-  }
-  if (error.status === 400) {
-    msg = "bad request";
-    icon = <ThumbDownAlt sx={{marginBottom: 2}} fontSize="large"/>;
-  }
-  if (error.status === 500) {
-    msg = "something's wrong on our end";
-    icon = <SentimentVeryDissatisfied sx={{marginBottom: 2}} fontSize="large"/>;
-  }
+    if (error) {
+        if (error.status === 404) {
+            msg = "we couldn't find that";
+            icon = <ExploreOff sx={{marginBottom: 3}} fontSize="large"/>;
+          }
+          if (error.status === 400) {
+            msg = "bad request";
+            icon = <ThumbDownAlt sx={{marginBottom: 2}} fontSize="large"/>;
+          }
+          if (error.status === 500) {
+            msg = "something's wrong on our end";
+            icon = <SentimentVeryDissatisfied sx={{marginBottom: 2}} fontSize="large"/>;
+          }
+    }
+  
   useEffect(() => {
     localStorage.removeItem("error")
   }, [])
-  
+
   return (
     <section className="error-container">
         {icon}
       <h2>oops! {error ? `${error.status}` : null}</h2>
-      {error ? `${msg}` : null}
+      {msg}
       
     </section>
   );
